9f0115a3cf3c4cf504bfaad2bef76607c814f9dc
jbmenashi <jbmenashi@gmail.com>
2018-11-19 09:55:45 -0500

    passed tests

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jacob"
+  end
+
+  get '/hometown' do
+    "My hometown is Newton, Mass"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Me and Julio down by the schoolyard" 
+  end
 end
